d4a3c2efb5be94025a8c1321ea5c17f1dd22a9c3
hasanul banna <bannawandoor@gmail.com>
2022-12-25 11:46:19 +0530

    clock and dynamic statisstics added in admin side

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login',views.admin_login,name='admin_login'),
     path('dashboard',views.admin_dashboard,name='admin_dashboard'),
     path('logout/', views.admin_logout, name='admin_logout'),
+    path('dashboard/<slug:month>', views.admin_dashboard_monthwise,name='admin_dashboard_monthwise')
     
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render,redirect
 from django.contrib import messages, auth
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 # Create your views here.
 from django.contrib import messages
-import datetime
+from datetime import datetime,date,timedelta
 from accounts.models import *
 from orders.models import *
+import calendar
 def admin_login(request):
       if request.method == 'POST':
         email = request.POST['email']
@@ -27,15 +29,52 @@ def admin_login(request):
 
 def admin_dashboard(request):
     customers_count = Account.objects.filter(is_admin=False).count()
-    orders_count = Order.objects.all().count()
+    orders_count = Order.objects.filter(is_ordered=True).count()
     product_count = Product.objects.filter(is_available=True).count()
+    total_orders = Order.objects.filter(is_ordered=True)
+    total_sales = sum(list(map(lambda x : x.order_total,total_orders)))
+    today = datetime.today()
+    this_month = today.month
+    month_list = list(map(lambda x : calendar.month_name[x],range(1,this_month+1)))
+    
 
-    return render(request,'admin_panel/admin_dashboard.html')
 
-@login_required(login_url='admin_login')
+
+    context = {
+        'total_customers' : customers_count,
+        'total_orders'    : orders_count,
+        'total_products'  : product_count,
+        'total_sales'     : total_sales,
+        'month_list'      : month_list
+    }
+    return render(request,'admin_panel/admin_dashboard.html',context)
+
+@staff_member_required(login_url='admin_login')
 def admin_logout(request):
     if 'email' in request.session:
         request.session.flush()
     auth.logout(request)
     messages.success(request, "You are logged out.")
     return redirect('admin_login')
+
+def admin_dashboard_monthwise(request,month):
+    today = datetime.today()
+    year = today.year
+    selected_month = month
+    month = datetime.strptime(month, '%B').month
+    customers_count = Account.objects.filter(is_admin=False,date_jointed__year= year,date_jointed__month=month).count()
+    orders_count = Order.objects.filter(is_ordered=True,created_at__year = year,created_at__month=month,).count()
+    product_count = Product.objects.filter(is_available=True,created_date__year=year,created_date__month=month).count()
+    total_orders = Order.objects.filter(is_ordered=True,created_at__year = year,created_at__month=month,)
+    total_sales = sum(list(map(lambda x : x.order_total,total_orders)))
+    month_list = list(map(lambda x : calendar.month_name[x],range(1,today.month+1)))
+    context = {
+        'total_customers' : customers_count,
+        'total_orders'    : orders_count,
+        'total_products'  : product_count,
+        'total_sales'     : total_sales,
+        'month_list'      : month_list,
+        'selected_month'  : selected_month
+    }
+    
+    return render(request,'admin_panel/admin_dashboard.html',context)
