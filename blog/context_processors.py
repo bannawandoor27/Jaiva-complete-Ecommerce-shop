0cd6636ee810968c801e64d2b72fd4dc1b546426
hasanul banna <bannawandoor@gmail.com>
2022-12-29 15:51:55 +0530

    minor updations in add blog view

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,5 +1,5 @@
 from .models import Blog
 
 def blog_data(request):
-    blog_data = Blog.objects.all().order_by('-modified_date')[:3]
-    return dict(blog_data=blog_data)
+    blog_data = Blog.objects.all().order_by('-modified_date')
+    return dict(blog_data=blog_data[:3],all_new_blogs=blog_data)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
+from timeago import format
 # Create your models here.
 class Blog(models.Model):
 
@@ -8,3 +9,7 @@ class Blog(models.Model):
     category = models.CharField(max_length=50,default = 'general')
     description = models.TextField(max_length=10000, blank=True)
     image = models.ImageField(upload_to='photos/blog', blank=False)
+    def time_ago(self):
+        now = timezone.now()
+        time_diff = format(self.modified_date,now)
+        return time_diff
